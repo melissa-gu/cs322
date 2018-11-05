26e95dc507d815b3919efdea8d638ac50325dcd1
Melissa Gu <melissa.gu@richmond.edu>
2018-11-05 13:41:28 -0500

    car program

--- Car.java
+++ Car.java
@@ -1,52 +1,54 @@
+import Intersection;
 public class Car {
-	private int ID;
-	private int intersectionID;
-	private Intersection intersectionReference;
-	private int timeLeftInSegment;
-	private int timeToTraverseSegment;
-	private int direction;
-	private String summary;
+    private int ID;
+    private int intersectionID;
+    private Intersection intersectionReference;
+    private int timeLeftInSegment;
+    private int timeToTraverseSegment;
+    private int direction;
+    private String summary;
 
-	// Constructor
-	public Car(int ID, int intersectionID, int timeToTraverseSegment, int direction) {
-		this.ID = ID;
-		this.intersectionID = intersectionID;
-		this.intersectionReference = ??;
-		this.timeToTraverseSegment = timeToTraverseSegment;
-		this.timeLeftInSegment = timeToTraverseSegment;
-		this.direction = direction;
-		this.summary = "";
-	}
+    // Constructor
+    public Car(int ID, int intersectionID, int timeToTraverseSegment, int direction) {
+        this.ID = ID;
+        this.intersectionID = intersectionID;
+        this.intersectionReference = ??;
+        this.timeToTraverseSegment = timeToTraverseSegment;
+        this.timeLeftInSegment = timeToTraverseSegment;
+        this.direction = direction;
+        this.summary = "";
+    }
 
-	// Update method
-	public void update() {
-		timeLeftInSegment--;
-	}
+    // Update method
+    public void update() {
+        timeLeftInSegment--;      
+    }
 
-	// Method that moves car to another intersection
-	public void moveToNextIntersection(int nextIntersectionID) {
-		appendToSummary("car#" + ID + " is removed from " + direction + 
-			            " queue of intersection " + car.intersectionReference + 
+    // Method that moves car to another intersection
+    public void moveToNextIntersection(int nextIntersectionID) {
+        appendToSummary("car#" + ID + " is removed from " + direction + 
+        	            " queue of intersection " + car.intersectionReference + 
                         " and placed into ");
-		intersectionID = nextIntersectionID;
-		// HOW DO WE GET THE ACTUAL INTERSECTION
-		addCarToQueue(ID);
-		timeLeftInSegment = timeToTraverseSegment;
-		appendToSummary(direction + "queue of intersection " + intersectionID);
+        intersectionID = nextIntersectionID;
+        // HOW DO WE GET THE ACTUAL INTERSECTION
+        intersectionReference.addCarToQueue(ID);
+        timeLeftInSegment = timeToTraverseSegment;
+        appendToSummary(direction + "queue of intersection " + intersectionID);
 
-	} 
+    } 
 
-	public boolean hasLeftGrid() {
-		return (interSectionID == -1 && timeLeftInSegment == 0);
-	}
+    public boolean hasLeftGrid() {
+        return (intersectionID == -1 && timeLeftInSegment == 0);
+    }
 
-	public void appendToSummary(String summary) {
-		this.summary.append(summary);
-	}
+    public void appendToSummary(String summary) {
+        this.summary.append(summary);
+    }
 
-	public String toString() {
-		String temp = this.summary;
-		this.summary = "";
-		return temp;
-	}
+    // Returns 
+    public String toString() {
+        String temp = this.summary;
+        this.summary = "";
+        return temp;
+    }
 }

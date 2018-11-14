fe0d1b8c58b1649359214622f9ce6a39c8de2742
Melissa Gu <melissa.gu@richmond.edu>
2018-11-14 16:08:51 -0500

    fixed multiple turns issue

--- Implementation/TrafficTesterView.java
+++ Implementation/TrafficTesterView.java
@@ -87,7 +87,7 @@ public class TrafficTesterView {
 
     // run the simulation (one time-step for now)
     // System.out.print(simulation.toString());
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 2; i++) {
       simulation.update();
       System.out.print(simulation.toString());
     } // end for
